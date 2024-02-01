561d3fd883bfb718fdf9b953f6f946739fef9f6e
Lavanya_23 <lavanyas0217@gmail.com>
2024-02-01 23:54:28 +0100

    Declaring global variables

--- source/Samples/NeoCortexApiExperiment/SpatialLearningExperiment.cs
+++ source/Samples/NeoCortexApiExperiment/SpatialLearningExperiment.cs
@@ -11,5 +11,24 @@ namespace NeoCortexApiExperiment
     /// </summary>
     internal class SpatialLearningExperiment
     {
+        /// We will use 200 bits to represent an input vector (pattern).
+        int inputBits = 200;
+
+        double max = 100;
+
+        /// <summary>
+        /// Implementing New Parameter in Dictionary for Scalar Encoder. 
+        /// </summary>
+        Dictionary<string, object> settings = new Dictionary<string, object>()
+            {
+                { "W", 5},
+                { "N", inputBits},
+                { "Radius", -1.0},
+                { "MinVal", 0.0},
+                { "Periodic", false},
+                { "Name", "scalar"},
+                { "ClipInput", false},
+                { "MaxVal", max}
+            };
     }
 }
