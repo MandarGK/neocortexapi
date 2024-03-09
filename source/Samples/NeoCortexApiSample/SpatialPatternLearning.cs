28df4e315f77721b6dd36b01382a04a77f03eedc
MandarGK <mandarkale5@gmail.com>
2024-03-09 19:29:45 +0100

    Changing Max Boost

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -26,7 +26,7 @@ namespace NeoCortexApiSample
             // Used as a boosting parameters
             // that ensure homeostatic plasticity effect.
             double minOctOverlapCycles = 1.0;
-            double maxBoost = 1.0;
+            double maxBoost = 5.0;
 
             // We will use 200 bits to represent an input vector (pattern).
             int inputBits = 200;
