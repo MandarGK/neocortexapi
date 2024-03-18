42f09eeb6d7445a7ade3027be0f5514e2171cc29
MandarGK <mandarkale5@gmail.com>
2024-03-18 19:13:05 +0100

    Code Refactoring and adding additional Comments for new Condition

--- source/Samples/NeoCortexApiExperiment/SpatialLearningExperiment.cs
+++ source/Samples/NeoCortexApiExperiment/SpatialLearningExperiment.cs
@@ -3,40 +3,61 @@ using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
 using NeoCortexApi.Network;
 using NeoCortexApi.Utility;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 
 namespace NeoCortexApiExperiment
 {
     /// <summary>
-    /// Implementng a new experiment that demonstrates Spatial pattern learining.
+    /// Implements an experiment that demonstrates how to learn spatial patterns.
+    /// SP will learn every presented input in multiple iterations.
     /// </summary>
     public class SpatialLearningExperiment
     {
-        public void Run ()
+        public void Run()
         {
-            Console.WriteLine($"NeocortexApi! New Spatial Learning Experiment {nameof(SpatialLearningExperiment)}");
-            int inputBits = 200;    
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(SpatialLearningExperiment)}");
 
-            double max = 100;
-
-
-            // Adding some parameter boosting Parameters 
-          
+            // Used as a boosting parameters
+            // that ensure homeostatic plasticity effect.
             double minOctOverlapCycles = 1.0;
             double maxBoost = 5.0;
 
-            // Using the Mini Columns we will create a slice of neocortex.
+            // We will use 200 bits to represent an input vector (pattern).
+            int inputBits = 200;
+
+            // We will build a slice of the cortex with the given number of mini-columns
             int numColumns = 1024;
 
+            //
+            // This is a set of configuration parameters used in the experiment.
+            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
+            {
+                CellsPerColumn = 10,
+                MaxBoost = maxBoost,
+                DutyCyclePeriod = 100,
+                MinPctOverlapDutyCycles = minOctOverlapCycles,
 
-            /// <summary>
-            /// Implementing New Parameter in Dictionary for Scalar Encoder. 
-            /// </summary>
+                GlobalInhibition = false,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * inputBits),
+                LocalAreaDensity = -1,
+                ActivationThreshold = 10,
+
+                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
+                Random = new ThreadSafeRandom(42),
+                StimulusThreshold = 10
+            };
+
+            double max = 100;
+
+            //
+            // This dictionary defines a set of typical encoder parameters.
             Dictionary<string, object> settings = new Dictionary<string, object>()
             {
                 { "W", 15},
@@ -49,31 +70,11 @@ namespace NeoCortexApiExperiment
                 { "MaxVal", max}
             };
 
-            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
-            {
-                CellsPerColumn = 10,
-                MaxBoost = maxBoost,
-                DutyCyclePeriod = 100,
-                MinPctOverlapDutyCycles = minOctOverlapCycles,
 
-
-                GlobalInhibition = false,
-                NumActiveColumnsPerInhArea = 0.02 * numColumns,
-                PotentialRadius = (int)(0.15 * inputBits),
-                LocalAreaDensity = -1,
-                ActivationThreshold = 10,
-
-                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
-                Random = new ThreadSafeRandom(42),
-                StimulusThreshold = 10,
-
-
-            };
-
-            //Implementing a encoder for this experiment
             EncoderBase encoder = new ScalarEncoder(settings);
 
-            //Creating 100 random variables as input
+            //
+            // We create here 100 random input values.
             List<double> inputValues = new List<double>();
 
             for (int i = 0; i < (int)max; i++)
@@ -82,16 +83,26 @@ namespace NeoCortexApiExperiment
             }
 
             var sp = RunExperiment(cfg, encoder, inputValues);
+
+            
         }
 
-
+        /// <summary>
+        /// Implements the experiment.
+        /// </summary>
+        /// <param name="cfg"></param>
+        /// <param name="encoder"></param>
+        /// <param name="inputValues"></param>
+        /// <returns>The trained bersion of the SP.</returns>
         private static SpatialPooler RunExperiment(HtmConfig cfg, EncoderBase encoder, List<double> inputValues)
         {
+            // Creates the htm memory.
             var mem = new Connections(cfg);
 
             bool isInStableState = false;
 
-            //Implementing New Method for Boosting
+            
+            //
             // HPC extends the default Spatial Pooler algorithm.
             // The purpose of HPC is to set the SP in the new-born stage at the begining of the learning process.
             // In this stage the boosting is very active, but the SP behaves instable. After this stage is over
@@ -99,69 +110,71 @@ namespace NeoCortexApiExperiment
             // Once the SDR generated for every input gets stable, the HPC will fire event that notifies your code
             // that SP is stable now.
             HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, inputValues.Count * 40,
-                (isStable, numPatterns, actColAvg, seenInputs) => {
-
-
+                (isStable, numPatterns, actColAvg, seenInputs) =>
+                {
+                    // Event should only be fired when entering the stable state.
+                    // Ideal SP should never enter unstable state after stable state.
                     if (isStable == false)
                     {
                         Debug.WriteLine($"INSTABLE STATE");
-                        //This should usually not happen
+                        // This should usually not happen.
                         isInStableState = false;
                     }
                     else
                     {
                         Debug.WriteLine($"STABLE STATE");
-                        //Entering in to Stable State
+                        // Here you can perform any action if required.
                         isInStableState = true;
                     }
-
                 });
 
+            // It creates the instance of Spatial Pooler Multithreaded version.
+            SpatialPooler sp = new SpatialPooler(hpa);
+            //sp = new SpatialPoolerMT(hpa);
 
-            //Creating the instance of Spatial Pooler Multithreaded version
-            SpatialPooler sp = new SpatialPooler();
-
-            //Initalizes the Spatial pooler
+            // Initializes the patial pooler
             sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
 
-            //Implementing Layers for Neocortex.
+            // mem.TraceProximalDendritePotential(true);
+
+            // It creates the instance of the neo-cortex layer.
+            // Algorithm will be performed inside of that layer.
             CortexLayer<object, object> cortexLayer = new CortexLayer<object, object>("L1");
 
-            // Encoder will receive the input and forward the encoded signal to the next module.
+            // Add encoder as the very first module. This model is connected to the sensory input cells
+            // that receive the input. Encoder will receive the input and forward the encoded signal
+            // to the next module.
             cortexLayer.HtmModules.Add("encoder", encoder);
 
-            // This Module will use the Output From Encoder and Build Sparse Distributed Representation.
+            // The next module in the layer is Spatial Pooler. This module will receive the output of the
+            // encoder.
             cortexLayer.HtmModules.Add("sp", sp);
 
             double[] inputs = inputValues.ToArray();
 
-            //Understanding the Input value in Array.
-            foreach (double value in inputs)
-            {
-                Console.WriteLine("Inside For each");
-                Console.WriteLine(value);
-            }
-
             // Will hold the SDR of every inputs.
             Dictionary<double, int[]> prevActiveCols = new Dictionary<double, int[]>();
 
-            // Will hold the similarity of SDKk and SDRk - 1 fro every input.
+            // Will hold the similarity of SDKk and SDRk-1 fro every input.
             Dictionary<double, double> prevSimilarity = new Dictionary<double, double>();
 
-            //Initialize start similarity to zero.
+            //
+            // Initiaize start similarity to zero.
             foreach (var input in inputs)
             {
                 prevSimilarity.Add(input, 0.0);
                 prevActiveCols.Add(input, new int[0]);
             }
 
+            // Learning process will take 1000 iterations (cycles)
             int maxSPLearningCycles = 1000;
 
-
+            //Intializing counter to break the loop after reaching stability
             int counter = 0;
 
             // Creating a Dictionary to store SDR values.
             Dictionary<int, List<List<int>>> SdrDictionary = new Dictionary<int, List<List<int>>>();
+
 
             for (int cycle = 0; cycle < maxSPLearningCycles; cycle++)
             {
@@ -174,12 +187,11 @@ namespace NeoCortexApiExperiment
 
                     // Learn the input pattern.
                     // Output lyrOut is the output of the last module in the layer.
-                    // 
                     var lyrOut = cortexLayer.Compute((object)input, true) as int[];
 
                     // This is a general way to get the SpatialPooler result from the layer.
                     var activeColumns = cortexLayer.GetResult("sp") as int[];
-                    
+
                     var actCols = activeColumns.OrderBy(c => c).ToArray();
 
                     similarity = MathHelpers.CalcArraySimilarity(activeColumns, prevActiveCols[input]);
@@ -195,13 +207,16 @@ namespace NeoCortexApiExperiment
                     SdrDictionary[Convert.ToInt32(input)].Add(actCols.ToList());
 
                     Debug.WriteLine($"[cycle={cycle.ToString("D4")}, StableCycles ={counter}, i={input}, cols={actCols.Length} s={similarity}] SDR: {string.Join(", ", SdrDictionary[Convert.ToInt32(input)].Last())}");
-
                     prevActiveCols[input] = activeColumns;
                     prevSimilarity[input] = similarity;
                 }
 
+
+                // Condition to check if the Spatial Pooler has Entered into Stable state.
                 if (isInStableState)
                 {
+
+                    // Incrementing the Counter to get the desire value to print the SDR for 100 Stable Cycles 
                     counter++;
                     Debug.WriteLine($"Counter Value: {counter}");
 
@@ -209,10 +224,9 @@ namespace NeoCortexApiExperiment
                     {
                         // Print the last 100 stable cycles.
                         int startCycle = Math.Max(0, cycle - 99); // Adjusted start cycle
-                        
-                        int endCycle = cycle; // End cycle is the current cycle
-                        
 
+                        // Checking the last 100th Cycle for the Iteration 
+                        int endCycle = cycle; // End cycle is the current cycle
 
                         for (int i = startCycle; i <= endCycle; i++)
                         {
@@ -244,18 +258,20 @@ namespace NeoCortexApiExperiment
                 }
                 else
                 {
+
+                    // Clearing All SDR Stored in Dictionary during the Instable state.
                     SdrDictionary.Clear();
+
+                    // Setting the counter to reset / 0 during the Instable state of Spatial pooler.
                     counter = 0;
                     Debug.WriteLine($"Counter is set to Zero Stability is not yet Reached");
                 }
 
-
             }
 
-
-                return sp;
+            return sp;
         }
+
+       
     }
-
 }
-
