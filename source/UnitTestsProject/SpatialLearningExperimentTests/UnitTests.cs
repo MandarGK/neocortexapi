0aa8bde2dc5dd53b2553d93e958bb9926e10f179
anushruth16 <anushruthpal@gmail.com>
2024-03-24 20:27:05 +0100

    Unit tests for jaccard similarity performed

---
+++ source/UnitTestsProject/SpatialLearningExperimentTests/UnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortex;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using NeoCortexApi;
+
+namespace UnitTestsProject.SimilarityUnitTests
+{
+    /// <summary>
+    /// Test for similarity calculation.
+    /// </summary>
+    [TestClass]
+    public class UnitTests
+    {
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 1, 2, 3, 4, 5, 6 }, 100.0)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 1, 2, 3, 4 }, 66.66666666666666)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 4, 5, 6 }, 50.0)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { }, -1.0)]
+        [DataRow(new int[] { }, new int[] { 1, 2, 3, 4, 5, 6 }, -1.0)]
+        [TestMethod]
+        [TestCategory("Prod")]
+        public void TestJaccardSimilarity(int[] arr1, int[] arr2, double expectedSimilarity)
+        {
+            double calculatedSimilarity = MathHelpers.JaccardSimilarity(arr1, arr2);
+
+            Assert.AreEqual(expectedSimilarity, calculatedSimilarity);
+
+            Console.WriteLine($"{calculatedSimilarity}");
+            Console.WriteLine($"{Helpers.StringifyVector(arr1)}");
+            Console.WriteLine($"{Helpers.StringifyVector(arr2)}");
+            Console.WriteLine();
+
+        }
+    }
+}
